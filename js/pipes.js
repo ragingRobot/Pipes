3bedd308c75528439bb1ee45a83eeb7a19d7f9ac
Josh Milstead <jmilst20@gmail.com>
2013-04-14 00:57:02 -0400

    added speed option and added win detection

--- js/pipes.js
+++ js/pipes.js
@@ -2,6 +2,14 @@ window.mobilecheck = function() {
 var check = false;
 (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
 return check; }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SETUP gameDataManager 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var gameOptionsManager = {
+	fillSpeed: 1500
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // SETUP Node OBJECTS
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -109,8 +117,9 @@ Pipe.prototype.fill = function(startConnectionIndex){
 		}
 		
 		console.log(this);
-		this.full = true;
+		
 		if(this._connectionStatus[startConnectionIndex] == 1){
+			this.full = true;
 			var thispipe = this;
 			
 			var animationOptions ={width:"71px"};
@@ -121,7 +130,7 @@ Pipe.prototype.fill = function(startConnectionIndex){
 				
 			}
 			
-			thispipe._htmlElement.find(".water").animate(animationOptions,2000, function(){
+			thispipe._htmlElement.find(".water").animate(animationOptions,gameOptionsManager.fillSpeed, function(){
 						thispipe._htmlElement.addClass("full");
 						for(var i =0 ; i < thispipe._connectionStatus.length ;i ++)
 						{
@@ -140,12 +149,17 @@ Pipe.prototype.fill = function(startConnectionIndex){
 							if(thispipe._connectionStatus[i] == 1 && i != startConnectionIndex){
 								if(thispipe._connections[i] != null && typeof thispipe._connections[i] != 'undefined'){
 									thispipe._connections[i].fill(from);
+								}else{
+									//lose :(
+									gameOptionsManager.fillSpeed = 100;
+									console.log("DEAD");
 								}
 							}
 						}
 			});
 		}else{
 			//lose :(
+				gameOptionsManager.fillSpeed = 100;
 				console.log("DEAD");
 		}
 	}
@@ -205,7 +219,11 @@ Pipe.prototype.setConnectionStatusList = function (connectionStatus)
 	
 }
 
-
+var WinPipe = function(){};
+WinPipe.prototype = new Pipe();
+WinPipe.prototype.fill = function(connectionIndex){
+	console.log("WIN");
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // SETUP GAME
@@ -214,11 +232,17 @@ Pipe.prototype.setConnectionStatusList = function (connectionStatus)
 var PipeGame = (function(){
 
 	
-	var _numCols = 5;
-	var _numRows = 5;
+	var _numCols = 8;
+	var _numRows = 4;
 	var _firstx = 0;
 	var _firsty = 0;
+	var _lastx = 3;
+	var _lasty = 0;
+	var _lastToFill;
 	var _firstToFill;
+	var _score = 0;
+	var _numberOfPipesFilled = 0;
+	var _fillSpeed = 3000;
 
 	function _configure(options){
 	/***************************************************************************************
@@ -229,7 +253,7 @@ var PipeGame = (function(){
 	_firstx = options.startX;
 	_firsty = options.StartY;	
 	}
-	
+
 	function _setGameBoard(){
 	/***************************************************************************************
 	 * This populates the game board based on the number of rows and cols specified.
@@ -243,7 +267,7 @@ var PipeGame = (function(){
 		var pipeclass;
 		var nodeAbove;
 		var bottomNode;
-		
+
 		for(var i=0; i< _numCols; i++)
 		{
 			
@@ -275,6 +299,11 @@ var PipeGame = (function(){
 				if(i == _firstx && j == _firsty){
 					
 					_firstToFill = bottomNode;
+			
+				}
+				if(i == _lastx && j == _lasty){
+					_lastToFill  = bottomNode;
+					_lastToFill.setConnection(1, new WinPipe());
 				}
 				
 				prevCol[j] = bottomNode;
@@ -292,17 +321,24 @@ var PipeGame = (function(){
 	
 	return{
 		setGameBoard:_setGameBoard,
-		configure:_configure
+		configure:_configure,
+		fillSpeed: _fillSpeed
 	}
 	
 })();
 
 $(document).ready(function(){
+
 	PipeGame.configure({
 		cols: 4,
 		rows: 6,
 		startX:0,
 		StartY:0	
 	});
+	
 	PipeGame.setGameBoard();
+	
+	$(".speedUp").click(function(){
+			gameOptionsManager.fillSpeed = 100;
+	});
 });
